c18c45bf3e1a5ce576d942cdae79a5394c8a99cd
allusurendrakumar <59111043+allusurendrakumar@users.noreply.github.com>
2019-12-23 16:35:22 +0530

    Add files via upload

---
+++ Pract.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Pract
+{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int s1=sc.nextInt();
+int s2=sc.nextInt();
+System.out.println("THE SUM OF"+" "+s1+" "+"and"+" "+s2+" "+"is"+(s1+s2));
+}
+}
---
+++ Pract4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Pract4
+{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int s1=sc.nextInt();
+if(s1>0){
+System.out.println("positive");
+}
+else if(s1<0){
+System.out.println("Negative");
+}
+else{
+System.out.println("Zero");
+}
+}
+}
